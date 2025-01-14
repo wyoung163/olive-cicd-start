9a1c45ca97b89efaacce8928a359f395e76cfb34
wyoung163 <choiwy0906@gmail.com>
2025-01-14 12:12:16 +0900

    docs: change workflow

--- src/test/java/me/dangtong/springbootdeveloper/SpringbootdeveloperApplicationTests.java
+++ src/test/java/me/dangtong/springbootdeveloper/SpringbootdeveloperApplicationTests.java
@@ -3,7 +3,7 @@ package me.dangtong.springbootdeveloper;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes=SpringbootdeveloperApplicationTests.class)
 class SpringbootdeveloperApplicationTests {
 
     @Test
